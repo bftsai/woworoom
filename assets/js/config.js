7d51921b83228c3847afe263096bb395458d838c
bf_tsai <tsai19911026@gmail.com>
2023-11-27 10:05:38 +0800

    docs: ✏️ initialize

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+const apiAdminUrl='https://livejs-api.hexschool.io/api/livejs/v1/admin/bftsai/'; //admin
+const apiCustomerUrl='https://livejs-api.hexschool.io/api/livejs/v1/customer/bftsai/'; //customer
+const uId='tkTk1S9IgGeDvn5yWsyd7skDl3x2';
+
